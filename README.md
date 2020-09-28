e1d6234c265f7abbf11e50a1dcd0e4a5f854f13f
Anna Melina Merkel <merkel@gsc.tu-darmstadt.de>
2020-09-28 12:19:37 +0200

    Added IGES file of rotor geometry of the constrained optimization (maintaining torque).

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 This repository contains data and Matlab/Octave code to generate the images from the paper "Shape Optimization of Rotating Electric Machines using Isogeometric Analysis and Harmonic Stator-Rotor Coupling" and the original and optimized rotor geometry
 
 * run "figures" in Octave/Matlab to generate the images from the paper
-* import IGES files "stator.igs", "rotor\_original.igs" and "rotor\_optimized.igs" to CAE software to see stator and rotor geometry
+* import IGES files "stator.igs", "rotor\_original.igs" (Fig. 6a), "rotor\_optimized.igs" (Fig. 6b) and "rotor\_optimized\_constrained.igs" (Fig. 10) to CAE software to see stator and rotor geometry
 
 This work is supported by the German BMBF by the PASIROM project (grant nr. 05M2018RDA), the Excellence Initiative of the German Federal and State Governments and by the Graduate School of Computational Engineering at Technische Universität Darmstadt.
--- figures.m
+++ figures.m
@@ -1,12 +1,3 @@
-% Figure 3
-data = csvread('data/figure03.csv');
-figure;
-loglog(data(:,1),data(:,2),'-o',data(3:10,1),data(3:10,3),'-o',data(5:10,1),data(5:10,4),'-o',data(7:10,1),data(7:10,5),'-o',data(:,1),data(:,6),'-o',data(3:10,1),data(3:10,7),'-o',data(5:10,1),data(5:10,8),'-o',data(7:10,1),data(7:10,9),'-o');
-legend('full problem, {\it N}_\Gamma=6','full problem, {\it N}_\Gamma=20','full problem, {\it N}_\Gamma=36','full problem, {\it N}_\Gamma=50','interface problem, {\it N}_\Gamma=6','interface problem, {\it N}_\Gamma=20','interface problem, {\it N}_\Gamma=36','interface problem, {\it N}_\Gamma=50');
-title('Figure 3');
-xlabel ('number of IGA degrees of freedom');
-ylabel ('computational time (s)');
-
 % Figure 8
 data = csvread('data/figure08.csv');
 figure;
@@ -26,11 +17,11 @@ xlabel ('harmonics');
 ylabel ('Fourier coefficients of EMF (V)');
 
 
-% Figure 10
-data = csvread('data/figure10.csv');
+% Figure 11
+data = csvread('data/figure11.csv');
 figure;
 bar(data(:,1),[data(:,2),data(:,3)]);
 legend('original design', 'optimized design');
-title('Figure 10');
+title('Figure 11');
 xlabel ('harmonics');
 ylabel ('Fourier coefficients of EMF (V)');
